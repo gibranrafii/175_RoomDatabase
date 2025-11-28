1442377b55575e0bbb2ab3adfe15a3841cf5006e
gibranrafii <gibranrafi21@gmail.com>
2025-11-28 10:06:32 +0700

    menambahkan interface RepositoriSiswa dan class offlineRepoSiswa

--- app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.praktikum7.repositori
 
+import com.example.praktikum7.room.Siswa
+import com.example.praktikum7.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun InsertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun InsertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
