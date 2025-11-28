473bb66e87a97280daf854ecd273bc629cd09171
gibranrafii <gibranrafi21@gmail.com>
2025-11-28 13:12:22 +0700

    melengkapi ContainerApp.ktr

--- app/src/main/java/com/example/praktikum7/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/praktikum7/repositori/ContainerApp.kt
@@ -1,2 +1,32 @@
 package com.example.praktikum7.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.praktikum7.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+
+class AplikasiSiswa : Application() {
+    /**
+     * Appcontainer instance digunakan oleh kelas-kelas lainnya untuk
+     * mendapatkan dependensi
+     */
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
