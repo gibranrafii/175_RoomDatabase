e11284ba9b98f635f4da9b42f3b36f96cbaa4d48
gibranrafii <gibranrafi21@gmail.com>
2025-11-28 10:05:02 +0700

    menambahkan abstract class pada DatabaseSiswa.kt

--- app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
@@ -1,4 +1,26 @@
 package com.example.praktikum7.room
 
-class DatabaseSiswa {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Siswa::class], version =  1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var instance: DatabaseSiswa? = null
+
+        fun getDatabase(context : Context): DatabaseSiswa{
+            return (instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also {instance=it }
+            })
+        }
+    }
 }
